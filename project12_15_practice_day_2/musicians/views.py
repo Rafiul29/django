3a33c88f861a7ded0951ac73bf04dcca851cbcf5
Rafiul29 <rafiulislam4472@gmail.com>
2024-05-30 22:20:46 +0600

    project done

--- project12_15_practice_day_2/musicians/views.py
+++ project12_15_practice_day_2/musicians/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import MusicianForm
+from . import models
 # Create your views here.
 
 def add_musician(request):
@@ -12,3 +13,15 @@ def add_musician(request):
     musician_form =MusicianForm()
   return render(request,'add_musician.html',{'form':musician_form})
  
+
+def edit_musician(request,id):
+  musician=models.Musician.objects.get(pk=id)
+  print(musician.email)
+  musician_form =MusicianForm(instance=musician)
+  if request.method=='POST':
+    musician_form =MusicianForm(request.POST,instance=musician)
+    if musician_form.is_valid():
+      # print(musician_form.cleaned_data)
+      musician_form.save()
+      return redirect('homepage')
+  return render(request,'add_musician.html',{'form':musician_form})
