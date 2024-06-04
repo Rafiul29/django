e601b57be9fc48fc869002919a67e58cf17fc38c
Rafiul29 <rafiulislam4472@gmail.com>
2024-06-04 22:42:45 +0600

    post category wise fiilter

--- project16_blog_post_curd_operation_Authentication/project10_blog_post_curd_operation/views.py
+++ project16_blog_post_curd_operation_Authentication/project10_blog_post_curd_operation/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,redirect
 from posts.models import Post
+from categories.models import Category
 
-def home(request):
+def home(request,category_slug=None):
+  categories=Category.objects.all()
   data=Post.objects.all()
-  return render(request,'home.html',{"data":data})
+  if category_slug is not None:
+    category=Category.objects.get(slug=category_slug)
+    data=Post.objects.filter(category=category)
+  return render(request,'home.html',{"data":data,'category':categories})
+
 
 
