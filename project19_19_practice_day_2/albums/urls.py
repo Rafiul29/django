3f3df43dc527a101c500e191abfef4ea4645c86c
Rafiul29 <rafiulislam4472@gmail.com>
2024-06-08 16:22:30 +0600

    all views class convert

--- project19_19_practice_day_2/albums/urls.py
+++ project19_19_practice_day_2/albums/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('add_album/',views.AddAlbumCreateView.as_view(),name='add_album'),
-    path('edit/<int:id>',views.edit_album,name='editalbum'),
-    path('delete/<int:id>',views.delete_album,name='deletealbum')
+    path('edit/<int:id>',views.EditAlbumView.as_view(),name='editalbum'),
+    path('delete/<int:id>',views.DeleteAlbumView.as_view(),name='deletealbum')
 ]
--- project19_19_practice_day_2/albums/views.py
+++ project19_19_practice_day_2/albums/views.py
@@ -6,18 +6,6 @@ from django.views.generic import CreateView,UpdateView,DeleteView,DetailView
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 
-# Create your views here.
-@login_required
-def add_album(request):
-  if request.method=='POST':
-    album_form =AlbumForm(request.POST)
-    if album_form.is_valid():
-      # print(album_form.cleaned_data)
-      album_form.save()
-      return redirect('homepage')
-  else:
-    album_form =AlbumForm()
-  return render(request,'add_album.html',{'form':album_form})
 
 @method_decorator(login_required ,name="dispatch")
 class AddAlbumCreateView(CreateView):
@@ -25,23 +13,23 @@ class AddAlbumCreateView(CreateView):
   form_class=AlbumForm
   template_name='add_album.html'
   success_url = reverse_lazy('homepage')
-
+  
   def form_valid(self,form):
       return super().form_valid(form)
-  
 
-@login_required
-def edit_album(request,id):
-  album=models.Album.objects.get(pk=id)
-  album_form=AlbumForm(instance=album)
-  if request.method=='POST':
-    album_form =AlbumForm(request.POST,instance=album)
-    if album_form.is_valid():
-      album_form.save()
-      return redirect("homepage")
-  return render(request,'add_album.html',{'form':album_form})
 
-@login_required
-def delete_album(request,id):
-  form=models.Album.objects.get(pk=id).delete()
-  return redirect('homepage')
+@method_decorator(login_required ,name="dispatch")
+class EditAlbumView(UpdateView):
+  model=models.Album
+  form_class=AlbumForm
+  template_name='add_album.html'
+  pk_url_kwarg='id'
+  success_url = reverse_lazy('homepage')
+
+
+@method_decorator(login_required ,name="dispatch")
+class DeleteAlbumView(DeleteView):
+  model=models.Album
+  pk_url_kwarg='id'
+  template_name='delete.html'
+  success_url = reverse_lazy('homepage')
